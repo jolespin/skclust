302e00e8897966462231738626af1aaad70053fe
Josh L. Espinoza <jol.espinoz@gmail.com>
2026-08-03 15:55:17 -0700

    Added r_squared to ClusteredDistances class

--- skclust/__init__.py
+++ skclust/__init__.py
@@ -11,7 +11,7 @@ advanced tree cutting, visualization, and network analysis capabilities.
 Author: Josh L. Espinoza
 """
 
-__version__ = "2026.6.22"
+__version__ = "2026.8.3"
 __author__ = "Josh L. Espinoza"
 
 from . import utils
